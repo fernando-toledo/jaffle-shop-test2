d3c06ea7bb01e1ae4e626a42d8632273beccfbc8
Fernando Toledo <fernando.toledo@dbtlabs.com>
2026-01-20 11:28:34 +0000

    add-macro

---
+++ macros/sf_table.sql
@@ -0,0 +1,26 @@
+{% materialization table, adapter='salesforce', supported_languages=['sql'] %}
+
+  {%- set identifier = model['alias'] -%}
+
+  {%- set target_relation = api.Relation.create(
+	identifier=identifier,
+	schema=schema,
+	database=database,
+	type='table'
+   ) -%}
+
+  {# Though adapter.execute supports passing the Arrow ADBC Statement options, it is not recommended to use in any user defined macros. #}
+  {% do adapter.execute(
+    sql=compiled_code,
+    auto_begin=False,
+    fetch=False,
+    limit=None,
+    options={
+      "adbc.salesforce.dc.dlo.primary_key": (config.get('primary_key', default=[]) | join(' ')),
+      "adbc.salesforce.dc.dlo.category": config.get('category', default='Profile'),
+      "adbc.salesforce.dc.dlo.target_dlo": identifier
+    }) %}
+
+  {{ return({'relations': [target_relation]}) }}
+
+{% endmaterialization %}
